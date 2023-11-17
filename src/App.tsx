5af799d9a204cb3ceccd5d346f6a8963398509cb
William Groc <william.groc@gmail.com>
2023-11-17 19:02:47 +0100

    Fetch graphql data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { lazy, type Component } from 'solid-js';
 
-import { Route, Router } from '@solidjs/router';
+import { A, Route, Routes } from '@solidjs/router';
 
 const Home = lazy(() => import('./routes/Home'));
+const Login = lazy(() => import('./routes/Login'));
 
 const App: Component = () => {
   return (
-    <Router>
-      <Route path='/' component={Home} />
-    </Router>
+    <>
+      <nav>
+        <A href="/">Home</A>
+        <A href="/login">Login</A>
+      </nav>
+      <Routes>
+        <Route path='/' component={Home} />
+        <Route path='/login' component={Login} />
+      </Routes>
+    </>
   );
 };
 
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,31 +1,35 @@
 import { createGraphQLClient, gql } from "@solid-primitives/graphql";
 import { User } from "../models/user.model";
-import { For, Match, Switch } from "solid-js";
+import { For, Match, Switch, createEffect, createMemo } from "solid-js";
 
 const getAllUsersQuery = gql`
-  query {
-      users {
-          id
-          name
-      }
+  query Users {
+    users {
+      id
+      name
+    }
   }
 `;
 
-const newQuery = createGraphQLClient("https://foobar.com/v1/api");
+const newQuery = createGraphQLClient("http://localhost:4000");
 
 export default function () {
-  const [data] = newQuery<Partial<User>[]>(getAllUsersQuery)
+  const [data] = newQuery<{ users: Partial<User>[] }>(getAllUsersQuery);
+
+  createEffect(() => {
+    console.log(data());
+  })
 
   return (
     <div>
       <h1>Home</h1>
       <Switch fallback={<div>Loading...</div>}>
-        <Match when={data.length === 0}>
+        <Match when={data()?.users?.length === 0}>
           <div>No users</div>
         </Match>
-        <Match when={data.length > 0}>
+        <Match when={data()?.users}>
           <div>Users</div>
-          <For each={data()}>
+          <For each={data()?.users}>
             {user => <div>{user.name}</div>}
           </For>
         </Match>
